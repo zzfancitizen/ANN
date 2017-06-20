7100b4b9b0d631c98a738b2bbd0b42c16d0357f8
zzfancitizen <zzfancitizen@gmail.com>
2017-06-20 10:37:37 +0800

    modify tuple to numpy ndarray

--- Perceptron.py
+++ Perceptron.py
@@ -1,32 +1,32 @@
 import numpy as np
-import functools
 
 
 class Perceptron(object):
     def __init__(self, input_num, activator):
         self.activator = activator
-        self.weights = np.zeros((1, input_num))[0]
+        self.weights = np.zeros((input_num, 1))
         self.bias = 0.0
 
     def __str__(self):
         return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
 
     def predict(self, input_vec):
-        return self.activator(functools.reduce(lambda a, b: a + b, map(lambda (x, w): x * w, zip(input_vec, self.weights)), 0.0) + self.bias)
+        return self.activator(input_vec.dot(self.weights) + self.bias)
 
     def train(self, input_vecs, labels, iteration, rate):
         for i in range(iteration):
+            print(i)
             self._one_iteration(input_vecs, labels, rate)
 
     def _one_iteration(self, input_vecs, labels, rate):
-        samples = zip(input_vecs, labels)
-        for (input_vec, label) in samples:
-            output = self.predict(input_vec)
-            self._update_weights(input_vec, output, label, rate)
+        for i in range(len(input_vecs)):
+            output = self.predict(input_vecs[i, :])
+            self._update_weights(input_vecs[i, :], output, labels[i, :], rate)
 
     def _update_weights(self, input_vec, output, label, rate):
         delta = label - output
-        self.weights = map(lambda (x, w): w + rate * delta * x, zip(input_vec, self.weights))
+        print(delta[0])
+        self.weights = self.weights + rate * delta * np.reshape(input_vec, (len(input_vec), 1))
         self.bias += rate * delta
 
 
@@ -35,21 +35,27 @@ def relu(x):
 
 
 def get_training_dataset():
-    input_vecs = [[1,1], [0,0], [1,0], [0,1]]
-    labels = [1, 0, 0, 0]
+    raw_vecs = [[1,1], [0,0], [1,0], [0,1]]
+    size_vecs = len(raw_vecs)
+    input_vecs = np.reshape(raw_vecs, (size_vecs, 2))
+
+    raw_labels = [1,0,0,0]
+    size_labels = len(raw_labels)
+    labels = np.reshape(raw_labels, (size_labels, 1))
     return input_vecs, labels
 
 
 def train_and_perceptron():
     p = Perceptron(2, relu)
     input_vecs, labels = get_training_dataset()
-    p.train(input_vecs, labels, 10, 0.1)
+    p.train(input_vecs, labels, 1000, 0.1)
     return p
 
 if __name__ == '__main__':
     and_perception = train_and_perceptron()
-    print (and_perception)
-    print ('1 and 1 = %d' % and_perception.predict([1, 1]))
-    print ('0 and 0 = %d' % and_perception.predict([0, 0]))
-    print ('1 and 0 = %d' % and_perception.predict([1, 0]))
-    print ('0 and 1 = %d' % and_perception.predict([0, 1]))
+    print(and_perception.predict(np.reshape([1,0], (1, 2)))[0][0])
+    # print (and_perception)
+    # print ('1 and 1 = %d' % and_perception.predict([1, 1]))
+    # print ('0 and 0 = %d' % and_perception.predict([0, 0]))
+    # print ('1 and 0 = %d' % and_perception.predict([1, 0]))
+    # print ('0 and 1 = %d' % and_perception.predict([0, 1]))
--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 import numpy as np
 
-input_vecs = [[1,1], [0,0], [1,0], [0,1]]
-weights = np.zeros((1, 2))[0]
 
-print(zip(input_vecs, weights).)
+def get_training_dataset():
+    raw_vecs = [[1,1], [0,0], [1,0], [0,1]]
+    size_vecs = len(raw_vecs)
+    input_vecs = np.reshape(raw_vecs, (size_vecs, 2))
+
+    raw_labels = [1,0,0,0]
+    size_labels = len(raw_labels)
+    labels = np.reshape(raw_labels, (size_labels, 1))
+    return input_vecs, labels
+
+
+x, y = get_training_dataset()
+
+z = np.reshape([0.1, 0.1], (2,1))
+
+print(x[1, :])
+print(z)
+print(x[1, :].dot(z))
+
+
 
