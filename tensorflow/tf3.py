726b876bb78ea7e27a35e1f3adbac6c26f358c12
zzfancitizen <zzfancitizen@gmail.com>
2017-08-01 18:13:31 +0800

    modify tuple to numpy ndarray

--- tensorflow/tf3.py
+++ tensorflow/tf3.py
@@ -7,7 +7,7 @@ one = tf.constant(1)
 new_value = tf.add(state, one)
 update = tf.assign(state, new_value)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
