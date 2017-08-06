ea43e7432615fe315de600867a4baf1cee63cb78
zzfancitizen <zzfancitizen@gmail.com>
2017-08-06 22:09:40 +0800

    msi

--- image.py
+++ image.py
@@ -1,12 +1,12 @@
 import cv2
 # from scipy import ndimage
 # from scipy import misc
-# # from PIL import Image
+# from PIL import Image
 import matplotlib.pyplot as plt
-import numpy as np
-import keras
-from keras.datasets import mnist
-from keras.datasets import cifar10
+# import numpy as np
+# import keras
+# from keras.datasets import mnist
+# from keras.datasets import cifar10
 
 # (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # (X_train_cf, y_train_cf), (X_test_cf, y_test_cf) = cifar10.load_data()
