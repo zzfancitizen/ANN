53722ca471acec982925de5b9055b9a612df0e36
zzfancitizen <zzfancitizen@gmail.com>
2017-08-07 16:29:56 +0800

    modify tuple to numpy ndarray

---
+++ tensorflow/tf7.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=3)
+
