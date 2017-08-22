5c5e24fb15afcd00f3857271d81f2adcd55548a2
zzfancitizen <zzfancitizen@gmail.com>
2017-08-22 16:22:10 +0800

    modify tuple to numpy ndarray

--- Evolution/test.py
+++ Evolution/test.py
@@ -1,11 +1,8 @@
 import numpy as np
 
-a = np.random.randint(0, 2, (1, 10)).repeat(10, axis=0)
-b = np.random.randint(0, 2, size=10).astype(np.bool)
+# a = np.random.randint(0, 2, (1, 10)).repeat(10, axis=0)
+# b = np.random.randint(0, 2, size=10).astype(np.bool)
+#
+# i_ = np.random.randint(0, 10, size=1)
 
-i_ = np.random.randint(0, 10, size=1)
-
-print(i_)
-print(b)
-print(a)
-print(a[i_, b])
+print(np.random.randint(*[32, 126], size=(300, 12)).astype(np.int8))
